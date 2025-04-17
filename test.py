743e63821ec8f40f92b96dfea615ead28591f94f
Noah <noah_molina@etu.u-bourgogne.fr>
2025-04-17 11:46:50 +0200

    détection

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import os
 import speech_recognition as sr
 from writting_retranscription import audio_transcribe
+from word_key_detection import key_word
 
 r=sr.Recognizer()
 mic=sr.Microphone()
@@ -10,11 +11,15 @@ with mic as source:
     print("Reglage du bruit ambiant...")
     r.adjust_for_ambient_noise(source)
     print("Parlez maintenant...")
-    audio = r.listen(source,timeout=5) #timeout pour eviter de bloquer le programme si on ne parle pas
+    audio = r.listen(source,timeout=100) #timeout pour eviter de bloquer le programme si on ne parle pas
     print("Fin de l'enregistrement.")
 with open("audio.wav", "wb") as f:
     f.write(audio.get_wav_data())
 
+
+
 resultat = audio_transcribe()
 print("Résultat de la transcription :")
 print(resultat)
+
+key_word(resultat)
