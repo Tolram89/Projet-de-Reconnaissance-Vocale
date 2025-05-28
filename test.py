9de0e573f01decdb3bc824df4107f94702a34ccb
Noah <noah_molina@etu.u-bourgogne.fr>
2025-05-28 08:31:39 +0200

    bouton_fenetre

--- test.py
+++ test.py
@@ -4,24 +4,24 @@ from writting_retranscription import audio_transcribe
 from vocal_retranscription import assistant_speech
 from word_key_detection import key_word
 
+def run_program():
+    r = sr.Recognizer()
+    mic = sr.Microphone()
+    #print("liste des microphones disponibles :")
+    #print(sr.Microphone.list_microphone_names()) #afin de trouver le bon index du micro
+    with mic as source:
+        print("Reglage du bruit ambiant...")
+        r.adjust_for_ambient_noise(source)
+        print("Parlez maintenant...")
+        audio = r.listen(source,timeout=5, phrase_time_limit=100) #timeout pour eviter de bloquer le programme si on ne parle pas
+        print("Fin de l'enregistrement.")
+    with open("audio.wav", "wb") as f:
+        f.write(audio.get_wav_data())
 
-r=sr.Recognizer()
-mic=sr.Microphone()
-#print("liste des microphones disponibles :")
-#print(sr.Microphone.list_microphone_names()) #afin de trouver le bon index du micro
-with mic as source:
-    print("Reglage du bruit ambiant...")
-    r.adjust_for_ambient_noise(source)
-    print("Parlez maintenant...")
-    audio = r.listen(source,timeout=5, phrase_time_limit=100) #timeout pour eviter de bloquer le programme si on ne parle pas
-    print("Fin de l'enregistrement.")
-with open("audio.wav", "wb") as f:
-    f.write(audio.get_wav_data())
 
 
-
-resultat = audio_transcribe()
-print("Résultat de la transcription :")
-print(resultat)
-reponse=key_word(resultat)
-assistant_speech(reponse)
+    resultat = audio_transcribe()
+    print("Résultat de la transcription :")
+    print(resultat)
+    reponse=key_word(resultat)
+    assistant_speech(reponse)
